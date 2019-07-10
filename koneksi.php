3548f33fc6bc00ec704a4a9fed457a3311a9997c
Meindika <43058655+meindika@users.noreply.github.com>
2019-07-10 14:20:31 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$koneksi = mysqli_connect("localhost","id9226201_multi_user","bucin12345","id9226201_multi_user");
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+ 
+?>
